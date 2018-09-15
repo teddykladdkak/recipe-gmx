6dace65ccc65f13c6ec028008814803719c0cc32
Mattias Lidbeck <teddykladdkak@gmail.com>
2018-09-15 04:43:44 +0200

    Changed the code and cleaned up
    - First of, where is the number in the element, that will tell you how much notifications you have. 1. Is it stored in the elements "value" ex. <span class="badge" value="67"></span> 2. Is it stored in the innerHMTL ex. <span class="badge">67</span>.
    - "Number()" is better to use than "parseInt()" but Franz can get setBadge() as string aswell.
    - Only get element once, makes cleaner code, and easyer to follow.
    - At the moment you get the badge number from the second element with class "badge" in the DOM.

--- webview.js
+++ webview.js
@@ -6,18 +6,23 @@ module.exports = (Franz) => {
     // getElementsByClassName returns an empty array; don't know why: that class 
     // name is clearly present several times in the DOM. Long line split for debugging.
     let els = document.getElementsByClassName('badge');
-    count = els.length;
-    let el = els[1];
-    let val = el.getAttribute('value');
-    if (document.getElementsByClassName('badge')[1].getAttribute('value') != null) {
+    /*count = els.length;*/ //This doesent make sense, why is number of elements with class "badge" on the page a number to setBadge?
+    let el = els[1]; //So you know that the second element in dom with class "badge" is the one with your badge number
+    /*let val = el.getAttribute('value');*/ //This is the value of what? Is the notification number in the elements value or innerHTML?
+    /*if (document.getElementsByClassName('badge')[1].getAttribute('value') != null) {
       count = parseInt(document.getElementsByClassName('badge')[1].innerHTML.replace(/[^0-9.]/g, ''), 10);
-    }
-
+    }*/
+    if(!el){
+      count = 0; //If element dont exist
+    }else{
+      count = Number(el.innerHTML.replace(/[^0-9.]/g, '')); //Use "Number()" instead of "parseInt();"
+    };
+    
     // Just in case we don't end up with a number, set it back to zero (parseInt can return NaN)
-    if (isNaN(count)) {
+    if (isNaN(count) || count == '') { //If count dont have any numbers, not only NaN
       count = 0;
     }
-
+    
     // set Franz badge
     Franz.setBadge(count);
   };
